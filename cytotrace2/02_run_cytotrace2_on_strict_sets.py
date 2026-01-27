211782383ec18788f4c14d47306ef27f7db0c53e
melissa-04 <mbmelisaa@gmail.com>
2026-01-27 22:18:09 +0300

    Create 02_run_cytotrace2_on_strict_sets.py

---
+++ cytotrace2/02_run_cytotrace2_on_strict_sets.py
@@ -0,0 +1,10 @@
+# ============================================
+# STEP13B — Install & import CytoTRACE2 (Python)
+# Package name on PyPI: cytotrace2-py
+# ============================================
+
+!pip -q install cytotrace2-py
+
+# # Import test (PyPI docs)
+from cytotrace2_py.cytotrace2_py import cytotrace2
+print("CytoTRACE2 import ✅")
